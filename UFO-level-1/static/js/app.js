96fcc330d393b9e14f6bc34409e331e7040af0a7
bbruzos <bbruzos@bu.edu>
2020-09-17 16:58:03 -0700

    2nd step

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,3 +5,21 @@ var tableData = data;
 
 var tbody = d3.select("tbody")
 console.log("First Step")
+
+// Use data entries as objects to grab key,value pairs from data
+// Loop through objects and place data in table area of html
+function dataDisplay(item){
+    tbody.text("");
+    item.forEach(ufo_sighting => {
+        console.table(ufo_sighting);
+        add_tr = tbody.append("tr");
+
+        Object.entries(ufo_sighting).forEach(function([key,value]){
+            add_td = add_tr.append("td").text(value);
+        });
+    });
+}
+
+// Perform the function on data to display on site
+dataDisplay(tableData)
+console.log("Second Step")
