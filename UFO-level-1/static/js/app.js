7af422959055c35eb54976bf2a1198684ec86a66
bbruzos <bbruzos@bu.edu>
2020-09-20 20:45:11 -0700

    filter input complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -35,6 +35,37 @@ d3.event.preventDefault();
 // Choose input element and grab raw HTML node
 // Then grab the value property of the input element
 var dateInput = d3.select("#datetime");
-var dateVal = 
-}
+var dateVal = dateInput.property("value");
+
+var cityInput = d3.select("#city");
+var cityVal = cityInput.property("value");
+
+var stateInput = d3.select("#state");
+var stateVal = stateInput.property("value");
+
+var countryInput = d3.select("#country");
+var countryVal = countryInput.property("value");
+
+var shapeInput = d3.select("#shape");
+var shapeVal = shapeInput.property("value");
+
+// show value of user inputs
+console.log(dateVal);
+console.log(cityVal);
+console.log(stateVal);
+console.log(countryVal);
+console.log(shapeVal);
+
+var tableFilter = tableData.filter(ufo_sighting =>{
+    return(ufo_sighting.datetime===dateVal || !dateVal) &&
+              (ufo_sighting.city===cityVal || !cityVal) &&
+              (ufo_sighting.state===stateVal || !stateVal) &&
+              (ufo_sighting===countryVal || !countryVal) &&
+              (ufo_sighting===shapeVal || !shapeVal )
+})
+
+dataDisplay(tableFilter);
+
+});
+
 
